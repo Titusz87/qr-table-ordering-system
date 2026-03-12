7159e712097212687f663c2b097eaa58cd6d1149
Titus <titus.tarcsai@gmail.com>
2026-03-12 16:47:53 +1030

    Add modular architecture: authorization-server, order-service, qr-service, client-application

--- authorization-server/src/main/java/org/restaurant/authorizationserver/AuthorizationServerConfig.java
+++ authorization-server/src/main/java/org/restaurant/authorizationserver/AuthorizationServerConfig.java
@@ -1,4 +1,30 @@
 package org.restaurant.authorizationserver;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import java.util.UUID;
+
+
+@Configuration
 public class AuthorizationServerConfig {
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registrarClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("registrar-client")
+                .clientSecret("{noop}secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .scope("client.create")
+                .scope("client.read")
+                .build();
+
+        return new InMemoryRegisteredClientRepository(registrarClient);
+    }
+
 }
