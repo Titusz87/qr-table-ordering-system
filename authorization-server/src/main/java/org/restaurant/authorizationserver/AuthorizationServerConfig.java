6494ed52b4109742b43a5a6617d16a04d9ac4530
Titus <titus.tarcsai@gmail.com>
2026-03-13 14:58:25 +1030

    Implements Authorization Server

--- authorization-server/src/main/java/org/restaurant/authorizationserver/AuthorizationServerConfig.java
+++ authorization-server/src/main/java/org/restaurant/authorizationserver/AuthorizationServerConfig.java
@@ -2,11 +2,19 @@ package org.restaurant.authorizationserver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
 import java.util.UUID;
 
 
@@ -15,9 +23,10 @@ public class AuthorizationServerConfig {
 
     @Bean
     public RegisteredClientRepository registeredClientRepository() {
+
         RegisteredClient registrarClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("registrar-client")
-                .clientSecret("{noop}secret")
+                .clientId("admin")
+                .clientSecret(encoder().encode("adminpassword"))
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
                 .scope("client.create")
@@ -26,5 +35,21 @@ public class AuthorizationServerConfig {
 
         return new InMemoryRegisteredClientRepository(registrarClient);
     }
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
 
+    @Bean
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .oauth2AuthorizationServer((authorizationServer) -> authorizationServer
+                        .oidc(Customizer.withDefaults())
+                );
+        return http.build();
+    }
 }
