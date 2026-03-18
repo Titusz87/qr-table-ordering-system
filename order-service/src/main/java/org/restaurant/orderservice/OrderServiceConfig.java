ab2c7b28869ae18a6c811ad9b1557ae2ae38b1e5
Titus <titus.tarcsai@gmail.com>
2026-03-18 19:52:10 +1030

    Sets auth configurations and implements the secrets.yaml to store secrets

--- order-service/src/main/java/org/restaurant/orderservice/OrderServiceConfig.java
+++ order-service/src/main/java/org/restaurant/orderservice/OrderServiceConfig.java
@@ -1,11 +1,10 @@
-package org.restaurant.orderservice.features;
+package org.restaurant.orderservice;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
