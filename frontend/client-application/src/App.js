f815af2d7be1e267b48069565366d8389c93d0d9
Titus <titus.tarcsai@gmail.com>
2026-03-24 11:55:42 +1030

    Refactors the project's structure

---
+++ frontend/client-application/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import AlignItemsList from './components/menu';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <AlignItemsList></AlignItemsList>
+      
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
